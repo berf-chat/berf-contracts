9841e3c747ed4c1be014eb6a9e498669a09a4bf0
RCantu92 <Roberto_Cantu@live.com>
2021-09-02 12:59:45 -0500

    Update NatSpec comments in BerfChatStorage.sol

--- contracts/BerfChatStorage.sol
+++ contracts/BerfChatStorage.sol
@@ -2,28 +2,41 @@ pragma solidity ^0.8.4;
 
 contract BerfChatStorage {
 
-    // Mapping storing an array of hashed messages
-    // mapped to a conversation id
+    /// Mapping storing an array of hashed messages
+    /// mapped to a conversation id.
     mapping(bytes => bytes[]) private conversation;
 
     event MessageSent(address from, address to);
 
-    // Hash the participant's addresses
-    // to use as a id for a specific chat
-    function hashAddresses(address _addr01, address _addr02) private {
-        bytes addressHash = keccak256(_addr01, _addr02);
-        Return addressHash;
+    /// @notice Takes two address and hashes them
+    /// together to create a chat id comprised of
+    /// the two chat participants.
+    /// @dev uses keccak256 as its hashing function.
+    /// @param _addr01 Address representing chat participant.
+    /// @param _addr02 Address representing chat participant.
+    /// @return Hash of the provided addresses.
+    function hashAddresses(address _addr01, address _addr02) private returns(bytes _addressHash) {
+        bytes _addressHash = keccak256(_addr01, _addr02);
+        return _addressHash;
     }
 
-    // Function to store message's hash
+    /// @notice Takes the message sent and stores it by
+    /// linking it to the chat id, comprised of the two
+    /// participant's addresses.
+    /// @param _to Address of the message recipient.
+    /// @param _message Hash of the intended message.
+    /// (the message has been hashed off-chain.)
     function storeMessage(address _to, bytes _message) public {
         bytes chatIdHash = hashAddresses(msg.sender, _to);
         conversation[chatIdHash].push(_message);
         emit MessageSent(msg.sender, _to);
     }
 
-    // Function that is called to pull
-    // relevant chat messages
+    /// @notice Pulls the hashes of the chat messages
+    /// as requested
+    /// @param _addr01 Address representing chat participant.
+    /// @param _addr02 Address representing chat participant.
+    /// @return _chat Hashes of the chat messages.
     function pullMessages(address _addr01, address _addr02) public view returns(bytes[] _chat) {
         bytes chatId = hashAddresses(_addr01,_addr02);
         bytes[] _chat = conversation[chatId];
